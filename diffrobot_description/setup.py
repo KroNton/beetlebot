3e89b6e3f61d40a44a3751a8dc5f21944bb206b0
KroNton <kyrlosfekry@gmail.com>
2024-09-13 02:35:45 +0300

    update pkg setup

--- diffrobot_description/setup.py
+++ diffrobot_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'diffrobot_description'
 
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.STL')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
