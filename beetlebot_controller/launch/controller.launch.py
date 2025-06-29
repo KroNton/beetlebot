9d0ee082e1f378c90ed81b6d529c8103a9685b01
KroNton <kyrlosfekry@gmail.com>
2025-06-29 21:08:42 +0300

    add launch file for joint state and velocity controllers

---
+++ beetlebot_controller/launch/controller.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager"
+        ]
+    )
+    
+    joint_velocity_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_velocity_controller",  
+            "--controller-manager",
+            "/controller_manager"
+        ]
+    )
+    
+    return LaunchDescription([
+        joint_state_broadcaster_spawner,
+        joint_velocity_controller
+    ])
